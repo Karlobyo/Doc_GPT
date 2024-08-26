70cd29ac5c04cbfd765cc6ea6862d388ba872657
carlobarbini <carlo.barbini90@gmail.com>
2024-08-26 15:05:51 +0200

    kickoff_clean

--- doc_gpt/api/fast.py
+++ doc_gpt/api/fast.py
@@ -19,7 +19,6 @@ app.add_middleware(
 
 app.state.model = load_summary_model()
 
-# http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
 @app.get("/predict")
 def predict(question: str):
     """
