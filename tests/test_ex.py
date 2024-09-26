4bc389d222615a4f0ab7190fc5d09cd6e99599de
carlobarbini <carlo.barbini90@gmail.com>
2024-09-26 11:03:25 +0200

    model test

--- tests/test_ex.py
+++ tests/test_ex.py
@@ -1,6 +1,13 @@
 import unittest
+from doc_gpt.api.fast import load_summary_model
 
 class TestEx(unittest.TestCase):
     def test_ex(self):
         # just a dummy test
         self.assertEqual(1, 1)
+
+
+    def model_exist(self):
+        # checking that the model is loaded
+        model = load_summary_model()
+        self.assertIsNotNone(model)
