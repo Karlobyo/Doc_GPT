db5cb86d95842b14c9fa0f2eb4ea2e282db70ea9
carlobarbini <carlo.barbini90@gmail.com>
2024-09-26 11:31:15 +0200

    mods

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -1,9 +1,13 @@
 import unittest
-from doc_gpt.dl_logic.registry import load_summary_model
+from doc_gpt.dl_logic.registry import load_summary_model, load_document_model, load_document_model_text
 
 class TestMod(unittest.TestCase):
 
     def test_mod(self):
         # checking that the model is loaded
-        model = load_summary_model()
-        self.assertIsNotNone(model)
+        model_sum = load_summary_model()
+        model_doc = load_document_model()
+        model_txt = load_document_model_text()
+        self.assertIsNotNone(model_sum)
+        self.assertIsNotNone(model_doc)
+        self.assertIsNotNone(model_txt)
