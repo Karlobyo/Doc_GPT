7b98b0bf4c3aa06beee72e3cfa2e9f695698fa34
carlobarbini <carlo.barbini90@gmail.com>
2024-09-26 11:21:01 +0200

    tests name

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -3,7 +3,7 @@ from doc_gpt.dl_logic.registry import load_summary_model
 
 class TestMod(unittest.TestCase):
 
-    def model_exist(self):
+    def test_mod(self):
         # checking that the model is loaded
         model = load_summary_model()
         self.assertIsNotNone(model)
